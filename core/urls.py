2b56daf1932522523670cb28532e74d6eee29670
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-01-28 03:37:02 +0000

    feat: add view and template for task detail

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, URLPattern, URLResolver
-from .views import TaskListView 
+from .views import TaskListView, TaskDetailView
 
 
 urlpatterns: list[URLPattern | URLResolver] = [
    path('', TaskListView.as_view(), name="tasks"),
+   path('task/<int:pk>', TaskDetailView.as_view(), name="task")
 ]
 
 # eosc
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Task
 
@@ -10,5 +11,8 @@ class TaskListView(ListView):
     context_object_name = 'tasks'
 
 
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = "task"
 
 # eosc
